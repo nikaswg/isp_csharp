bf73544faf850e772efcac5c1c55d41d9014bac2
Никита Нагорнюк <nikitanagornuik@gmail.com>
2025-02-14 13:42:44 +0300

    lab1 v1.0 добавлен метод вычисления функции f

---
+++ lab1/lab/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод параметров
+            Console.WriteLine("Введите xn и xk (через пробел):");
+            string[] xParams = Console.ReadLine().Split();
+            double xn = double.Parse(xParams[0]);
+            double xk = double.Parse(xParams[1]);
+
+            Console.WriteLine("Введите yn и yk (через пробел):");
+            string[] yParams = Console.ReadLine().Split();
+            double yn = double.Parse(yParams[0]);
+            double yk = double.Parse(yParams[1]);
+
+            Console.WriteLine("Введите количество частей (n):");
+            int n = int.Parse(Console.ReadLine());
+
+            // Вычисление шагов
+            double h = (xk - xn) / n;
+            double t = (yk - yn) / n;
+
+            // Создание экземпляра класса Calc
+            Calc calculator = new Calc();
+
+            // Заголовок таблицы
+            Console.WriteLine($"{"x",-10} {"f(x)",-10} {"y",-10} {"g(y)",-10}");
+
+            // Вычисление и вывод значений
+            for (double x = xn; x <= xk; x += h)
+            {
+                double fx = calculator.Calculate_f(x, xk, yn, yk, n, t); // Вызов метода
+                for (double y = yn; y <= yk; y += t)
+                {
+                    double gy = CalculateG(y); // Нужно определить CalculateG
+                    Console.WriteLine($"{x,-10:F2} {fx,-10:F2} {y,-10:F2} {gy,-10:F2}");
+                }
+            }
+        }
+
+        static double CalculateG(double y)
+        {
+            return Math.Sqrt(y); // Пример вычисления для g(y)
+        }
+    }
+}
---
+++ lab1/lab/calc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lab
+{
+    public class Calc
+    {
+        public double Calculate_f(double xn, double xk, double yn, double yk, double n, double t)
+        {
+            // Проверка входных параметров
+            if (n <= 0 || t <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Шаги n и t должны быть положительными.");
+            }
+
+            for (double i = xn; i < xk; i += n)
+            {
+                for (double j = yn; j < yk; j += t)
+                {
+                    if (i * j < 2)
+                    {
+                        return Math.Sin(i) + Math.Cos(j);
+                    }
+                    else if (i * j == 2)
+                    {
+                        return (i * i + 24) / Math.Pow((1 + i), 2);
+                    }
+                    else if (i * j > 2)
+                    {
+                        return Math.Cos(j) + 1 / Math.Sqrt(i * j);
+                    }
+                }
+            }
+
+            return 0; // Возврат значения по умолчанию
+        }
+
+
+    }
+}
